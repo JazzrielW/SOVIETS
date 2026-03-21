ca85926f14e4b0d61b1c57d75028f5bdc6500ec8
JazzrielW <61950251+JazzrielW@users.noreply.github.com>
2026-03-21 14:44:56 -0600

    ACTUALIZACION
    Se agregan varias entradas del Blog, la revuelta, y nueva sección de proyectos

---
+++ assets/js/1f9c2083.92bcc70d.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocusaurus=globalThis.webpackChunkdocusaurus||[]).push([[1155],{7969:s=>{s.exports=JSON.parse('{"title":"Editorial Soviets","description":"\xa1Tenemos una editorial! o algo as\xed.","link":"/blog/Editorial Soviets"}')}}]);
--- assets/js/5dee4e8a.8d321039.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdocusaurus=globalThis.webpackChunkdocusaurus||[]).push([[9054],{3083:o=>{o.exports=JSON.parse('{"title":"Donde pisa el desarrollador, no florece el trabajador","description":"Nota a prop\xf3sito del 4 de julio","link":"/blog/Lo llaman renovaci\xf3n, pero es gentrificaci\xf3n"}')}}]);
